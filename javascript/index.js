002950bc8c82c9f355aae22bc443ea0b479b0d11
Yonn-2k03 <dionpandu9613@gmail.com>
2022-12-01 22:39:39 +0700

    Add File

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+	    // toggle
+function toggle() {
+    // body...
+    var toggle = document.querySelector('.navbar-toggler');
+    var collapse = document.querySelector('.collapsed');
+    toggle.classList.toggle('active');
+    collapse.classList.toggle('active');
+}
